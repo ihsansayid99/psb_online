ab7071940e6b7f064a16160a905547f034f606fc
ihsansayid99 <ihsansayid66@gmail.com>
2021-10-15 12:04:02 +0700

    update

--- app/siswa-status-pendaftaran.php
+++ app/siswa-status-pendaftaran.php
@@ -2,7 +2,7 @@
 $id_session = $_SESSION['id_user'];
 $query = mysqli_query($db_conn, "SELECT * FROM pendaftaran WHERE id_user = '$id_session'");
 $data = mysqli_fetch_array($query);
-
+$row = mysqli_num_rows($query);
 //Data jurusan  
 $query2 = mysqli_query($db_conn, "SELECT p.minat_id_jurusan_1, j.nama_jurusan FROM pendaftaran p INNER JOIN jurusan j ON j.id = p.minat_id_jurusan_1");
 $data2 = mysqli_fetch_array($query2);
@@ -13,6 +13,9 @@ $data3 = mysqli_fetch_array($query3);
 <h2>Status Pendaftaran</h2>
 <hr class="border-menu">
 <div class="py-4">
+    <?php 
+        if($row > 0){
+    ?>
     <div class="card mx-auto" style="width: 18rem;">
         <img class="card-img-top" src="./images/<?php echo $data['foto'];  ?>" alt="Card image cap">
         <div class="card-body">
@@ -37,4 +40,10 @@ $data3 = mysqli_fetch_array($query3);
             <?php } ?>
         </div>
     </div>
+    <?php } else { ?>
+    <div class="text-center">
+        <h1>Silahkan isi form pendaftaran terlebih dahulu</h1>
+        <a href="index.php?p=siswa-pendaftaran" class="btn btn-primary">Daftar</a>
+    </div>
+    <?php } ?>
 </div>
